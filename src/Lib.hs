5fa927aa77631ae2c7dca957dcfd31046d3eb24f
Dantara <supercreed2@gmail.com>
2020-06-27 00:31:13 +0300

    Updated readme

--- src/Lib.hs
+++ src/Lib.hs
@@ -49,16 +49,16 @@ populationSize = 12
 parentsAmount = 2
 
 -- Amount of genetic algorithm iterations for one chunk
-gaN = 400
+gaN = 3500
 
 -- Background color of image
 baseBackgroundColor = PixelRGBA8 0 0 0 255
 
 -- Path to input image
-inputImagePath = "inputs/input.png"
+inputImagePath = "inputs/final3.png"
 
 -- Path to output image
-outputImagePath = "outputs/output.png"
+outputImagePath = "outputs/FINAL_3500.png"
 
 data Polygon = Polygon [Point] PixelRGBA8 deriving (Show, Generic, NFData)
 type Gene = Polygon
@@ -127,7 +127,7 @@ pixelDiff px1 px2 = abs (r1-r2) + abs (g1-g2) + abs (b1-b2)
     (PixelRGBA8 r1_ g1_ b1_ _) = px1
     (PixelRGBA8 r2_ g2_ b2_ _) = px2
 
-imageDiff :: Image PixelRGBA8 -> Image PixelRGBA8 -> (Int, Int)-> Integer
+imageDiff :: Image PixelRGBA8 -> Image PixelRGBA8 -> (Int, Int) -> Integer
 imageDiff img1 img2 (x_max, y_max) = sum $ do
   x <- [0..(x_max-1)]
   y <- [0..(y_max-1)]
